7b7f1c1f188dabbff6f49ef7b334ada4297f95f6
smogee7 <smogee6@gmail.com>
2025-06-26 13:44:55 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMINS = list(map(int, os.getenv("ADMINS", "").split(",")))
---
+++ main.py
@@ -0,0 +1,24 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from aiogram.utils import executor
+
+from config import BOT_TOKEN
+from keyboards.menu import get_main_keyboard
+from utils.auth import is_admin
+
+from handlers import user, admin
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start_handler(message: Message):
+    keyboard = get_main_keyboard(is_admin(message.from_user.id))
+    await message.answer("Добро пожаловать!", reply_markup=keyboard)
+
+# Регистрируем обработчики
+user.register_handlers(dp)
+admin.register_handlers(dp)
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
