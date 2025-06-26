db8c4f8a80b1cf67ce26025cc854537c91603c2b
smogee7 <smogee6@gmail.com>
2025-06-26 14:57:39 +0300

    1pdate main.py

--- main.py
+++ main.py
@@ -6,19 +6,18 @@ from config import BOT_TOKEN
 from keyboards.menu import get_main_keyboard
 from utils.auth import is_admin
 
-from handlers import start, user, admin  # Объединённый импорт
+from handlers import user, admin, support
 
-# Сначала создаём бота и диспетчер
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
-# Обработчик команды /start
 @dp.message_handler(commands=['start'])
 async def start_handler(message: Message):
     keyboard = get_main_keyboard(is_admin(message.from_user.id))
     await message.answer("Добро пожаловать!", reply_markup=keyboard)
 
 # Регистрируем обработчики
+support.register_handlers(dp)
 start.register_handlers(dp)
 user.register_handlers(dp)
 admin.register_handlers(dp)
