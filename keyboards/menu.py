d8c17609f83ec0879dc748a03a730e15dd022333
smogee7 <smogee6@gmail.com>
2025-06-26 15:05:35 +0300

    Update menu.py

--- keyboards/menu.py
+++ keyboards/menu.py
@@ -1,4 +1,4 @@
-    from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def get_main_keyboard(is_admin: bool) -> ReplyKeyboardMarkup:
     buttons = [
